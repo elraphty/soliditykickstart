1364a92a3f05915cc34fe75982188999dbb1ac5c
elraphty <elraphtyofficial@gmail.com>
2021-12-17 23:33:12 +0100

    created kickstartfactory/kickstart contract with variables

---
+++ ethereum/contracts/Kickstart.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.2 <0.9.0;
+
+contract Kickstart {
+    struct Request   {
+      string  description;
+      uint value;
+      address payable recipient;
+      bool complete;
+      uint approvalCount;
+      mapping(address => bool) approvals;
+    }
+    
+    mapping(uint => Request) public requests;
+    uint private currentIndex;
+    
+    address payable public manager;
+    uint public minimumContribution;
+    mapping(address  => bool) public approvers;
+    uint public approversCount;
+
+    constructor(uint minimum, address payable creator) {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+}
---
+++ ethereum/contracts/KickstartFactory.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.2 <0.9.0;
+import "./Kickstart.sol";
+
+contract KickstartFactory {
+    address[] public deployedCampaigns;
+    
+    function createCampaign(uint minimum) public {
+        address newCampaign = address(new Kickstart(minimum, payable(msg.sender)));
+        
+        deployedCampaigns.push(newCampaign);
+    }
+    
+    function getDeployedCampaigns() public view returns (address[] memory){
+        return deployedCampaigns;
+    }
+}
+
+
+
+
