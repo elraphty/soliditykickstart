45aadc8ecb05107096b89553029e427ae406ebf8
elraphty <elraphtyofficial@gmail.com>
2021-12-18 00:50:03 +0100

    created contract deploy  functionality

---
+++ ethereum/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const compiledFactory = require('./build/KickstartFactory.json');
+require('dotenv').config();
+
+const provider = new HDWalletProvider(
+  process.env.WALLET_MNEMONIC,
+  process.env.INFURA_ENDPOINT
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy using account ' + accounts[0]);
+
+  const txn = await new web3.eth.Contract(compiledFactory.abi)
+    .deploy({ data: compiledFactory.evm.bytecode.object })
+    .send({ from: accounts[0] });
+
+  console.log('Contract is at ' + txn.options.address);
+  provider.engine.stop();
+};
+
+deploy();
